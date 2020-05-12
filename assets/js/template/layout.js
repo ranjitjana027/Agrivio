5bc340ea97d14844fbf950cf480c75e804dfc039
Ranjit Jana <ranjitjana027@gmail.com>
2020-05-12 22:54:37 +0530

    article css v2, layout css v2

---
+++ assets/js/template/layout.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  /* show /hide navigation list */
+  window.addEventListener("click",evt=>{
+    if(evt.target.classList!=undefined && evt.target.classList.contains("menu-icon")){
+      var user_nav_list=document.querySelector("div.user-nav ul");
+      if(user_nav_list.classList.contains("show-nav")){
+        user_nav_list.classList.remove("show-nav");
+      }
+      else{
+        user_nav_list.classList.add("show-nav");
+      }
+    }
+    else{
+      document.querySelector("div.user-nav ul").classList.remove("show-nav");
+    }
+  });
+
+  /* show notifications */
+  window.addEventListener("click",evt=>{
+    if( (evt.target.parentNode!=null &&
+      evt.target.parentNode.classList!=undefined &&
+       evt.target.parentNode.classList.contains("bell-icon"))
+      || (evt.target.parentNode.parentNode!=null &&
+        evt.target.parentNode.parentNode.classList!=undefined &&
+        evt.target.parentNode.parentNode.classList.contains("bell-icon")))
+      {
+        var target=document.querySelector(".notification-list");
+        if(target.classList.contains("hidden")){
+          target.classList.remove("hidden");
+        }
+        else{
+          target.classList.add("hidden");
+        }
+      }
+      else{
+        document.querySelector(".notification-list").classList.add("hidden");
+      }
+
+  });
+
+  /* show account action list */
+  window.addEventListener("click",evt=>{
+    if(evt.target.parentNode!=null && evt.target.parentNode.classList!=undefined && evt.target.parentNode.classList.contains("user-icon")){
+      var target=document.querySelector(".account-nav");
+      if(target.classList.contains("hidden")){
+        target.classList.remove("hidden");
+      }
+      else{
+        target.classList.add("hidden");
+      }
+    }
+    else{
+      document.querySelector(".account-nav").classList.add("hidden");
+    }
+  });
+
+  
+});
